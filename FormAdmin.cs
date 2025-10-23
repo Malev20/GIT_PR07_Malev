d16c2534a40f26e83811b49d3c0a09c2042926a0
Malev <nikitozzz.m2005@mail.ru>
2025-10-23 20:25:47 +0300

    Добавлены элементы и отображение даты/времени

--- FormAdmin.cs
+++ FormAdmin.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void FormAdmin_Load(object sender, EventArgs e)
+        {
+            label3.Text = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+        }
     }
 }
